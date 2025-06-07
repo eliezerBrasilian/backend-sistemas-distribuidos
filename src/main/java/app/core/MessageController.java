31935acdae6f3388c4253bf0fe868c6594eb2ea8
Camille Paiva <camille@wwcalc.com>
2025-06-07 20:51:15 -0300

    enviando mensagem padrozinada

--- src/main/java/app/core/MessageController.java
+++ src/main/java/app/core/MessageController.java
@@ -1,10 +1,12 @@
 package app.core;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RequestMapping("api/votar")
 @RestController
@@ -16,10 +18,12 @@ public class MessageController {
         this.sender = sender;
     }
 
-    @PostMapping
-    public ResponseEntity<String> send(
+    @PostMapping("/candidato/{id}")
+    public ResponseEntity<String> votar(
+        @PathVariable("id") String idCandidato,
         @RequestBody Message body
     ) {
+        System.out.println("votou em: " + idCandidato);
         sender.sendMessage(body);
         return ResponseEntity.ok("Mensagem enviada com sucesso");
     }
