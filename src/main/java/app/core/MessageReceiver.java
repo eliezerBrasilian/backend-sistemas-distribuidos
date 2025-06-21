b7404eb96e00273f9b91e13eea57bdb00775aad6
EliezerBrasilian <alpinistamestre@yahoo.com>
2025-06-20 22:23:46 -0300

    corrigido erro do datetime

--- src/main/java/app/core/MessageReceiver.java
+++ src/main/java/app/core/MessageReceiver.java
@@ -3,21 +3,31 @@ package app.core;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import app.core.MessageController.PayloadCore;
 import app.core.websocket.SimpleWebSocketHandler;
 
 @Component
 public class MessageReceiver {
     final SimpleWebSocketHandler socketHandler;
+    final ObjectMapper objectMapper;
 
-    public MessageReceiver(SimpleWebSocketHandler socketHandler) {
+    public MessageReceiver(SimpleWebSocketHandler socketHandler,
+            ObjectMapper objectMapper) {
         this.socketHandler = socketHandler;
+        this.objectMapper = objectMapper;
     }
 
     @RabbitListener(queues = "backend-response-queue")
     public void handleResponse(
-            String message) {
+            PayloadCore message) throws JsonProcessingException {
         System.out.println("📥 Resposta recebida do core: " + message);
-        socketHandler.broadcastMessage(message);
+
+        String str = objectMapper.writeValueAsString(message);
+
+        socketHandler.broadcastMessage(str);
     }
 
 }
--- src/main/java/app/core/RabbitConfig.java
+++ src/main/java/app/core/RabbitConfig.java
@@ -1,13 +1,19 @@
 package app.core;
 
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.rabbit.listener.RabbitListenerContainerFactory;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 @Configuration
 public class RabbitConfig {
@@ -25,8 +31,8 @@ public class RabbitConfig {
     }
 
     @Bean
-    public MessageConverter messageConverter() {
-        return new Jackson2JsonMessageConverter();
+    public MessageConverter messageConverter(ObjectMapper objectMapper) {
+        return new Jackson2JsonMessageConverter(objectMapper);
     }
 
     @Bean
@@ -35,6 +41,25 @@ public class RabbitConfig {
         RabbitTemplate template = new RabbitTemplate(connectionFactory);
         template.setMessageConverter(messageConverter);
         return template;
+    }
+
+    @Bean
+    public RabbitListenerContainerFactory<?> rabbitListenerContainerFactory(
+            ConnectionFactory connectionFactory,
+            MessageConverter messageConverter) {
+
+        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
+        factory.setMessageConverter(messageConverter);
+        return factory;
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        return mapper;
     }
 
 }
