4321d0f03f356355e2294cddf61365b3077d2d40
EliezerBrasilian <alpinistamestre@yahoo.com>
2025-06-20 21:01:09 -0300

    removido objectMapper

--- src/main/java/app/core/MessageSender.java
+++ src/main/java/app/core/MessageSender.java
@@ -4,31 +4,21 @@ import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 @Component
 public class MessageSender {
     final RabbitTemplate rabbitTemplate;
     final String fila;
-    ObjectMapper objectMapper;
 
     public MessageSender(
             RabbitTemplate rabbitTemplate,
-            @Value("${core.queue}") String fila,
-            ObjectMapper objectMapper) {
+            @Value("${core.queue}") String fila) {
         this.rabbitTemplate = rabbitTemplate;
         this.fila = fila;
-        this.objectMapper = objectMapper;
     }
 
     public void sendMessage(MessageController.PayloadCore message) {
         try {
-            String json = objectMapper.writeValueAsString(message);
-
-            System.out.println("fila: " + fila);
-            System.out.println("json");
-            System.out.println(json);
-            rabbitTemplate.convertAndSend(fila, json);
+            rabbitTemplate.convertAndSend(fila, message);
             System.out.println("enviado");
         } catch (Exception e) {
             // failover
